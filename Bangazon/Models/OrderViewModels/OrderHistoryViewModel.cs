c2c26df5e32880d029bc223eb918d01c9e030dab
Michael Yankura <myankura210@gmail.com>
2019-07-19 15:37:17 -0500

    Not done.

---
+++ Bangazon/Models/OrderViewModels/OrderHistoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.OrderViewModels
+{
+    public class OrderHistoryViewModel
+    {
+        public Order Order { get; set; }
+
+        public List<Order> PreviousOrders { get; set; }
+
+        
+    }
+}
